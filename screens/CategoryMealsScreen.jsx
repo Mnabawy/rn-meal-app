f532319191a2521ad13418f05e16ffadc3c15916
Mnabawy <m_ninja61@yahoo.com>
2021-12-11 17:16:38 +0200

    screens

---
+++ screens/CategoryMealsScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native"
+
+const CategoryMealsScreen = props => {
+  return (
+    <View>
+      <Text>Category Meals Screen</Text>
+    </View>
+  )
+}
+
+export default CategoryMealsScreen
+
+const styles = StyleSheet.create({})
---
+++ screens/FavoritesScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native"
+
+const FavoritesScreen = props => {
+  return (
+    <View>
+      <Text>Favorite Screen</Text>
+    </View>
+  )
+}
+
+export default FavoritesScreen
+
+const styles = StyleSheet.create({})
