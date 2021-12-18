da522e1d6d0ad9de21ff45867d4cefab4026ddcc
Mnabawy <m_ninja61@yahoo.com>
2021-12-18 12:45:28 +0200

    favorite with dummy data

--- screens/FavoritesScreen.jsx
+++ screens/FavoritesScreen.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
+import MealList from "../components/MealList"
+import { MEALS } from "../data/dummy-data"
 
 const FavoritesScreen = props => {
-  return (
-    <View style={styles.screen}>
-      <Text>Favorite Screen</Text>
-    </View>
+  const favoriteListData = MEALS.filter(
+    meal => meal.id === "m1" || meal.id === "m2"
   )
+  return <MealList listData={favoriteListData} navigation={props.navigation} />
 }
 
 const styles = StyleSheet.create({
